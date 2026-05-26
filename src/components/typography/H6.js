6ca14477dc95ca1acfddbdc0480988197cfc857a
busrakuscu0 <busrakuscu6991@gmail.com>
2026-05-26 11:59:14 +0200

    fix: correct return statements in H3 and H6 components

--- src/components/typography/H6.js
+++ src/components/typography/H6.js
@@ -6,17 +6,17 @@ export function H6({ element, children }) {
           {children}
         </p>
       );
-      break;
     case "span":
       return (
         <span className="text-md font-semibold text-slate-900 self-end">
           {children}
         </span>
       );
-      break;
     default:
-      <h6 className="text-md font-semibold text-slate-900 self-end">
-        {children}
-      </h6>;
+      return (
+        <h6 className="text-md font-semibold text-slate-900 self-end">
+          {children}
+        </h6>
+      );
   }
 }
