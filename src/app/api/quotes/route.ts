2afeb150261dd9be65e5a44f8980b9711ca2061d
busrakuscu0 <busrakuscu6991@gmail.com>
2026-07-26 18:10:12 +0200

    feat: implement seed functionality to insert initial quotes into MongoDB

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,15 +1,11 @@
 import { Collections, getDb } from "@/lib/db";
 import { Quote } from "@/types/quotes";
-import { quotes } from "@/quotes";
 
 export async function GET() {
   const db = await getDb();
   const col = db.collection<Quote>(Collections.quotes);
 
-  const result = await col.insertMany(quotes);
-
-  const query = { adminApproved: false };
-  //const quotes = await col.find(query).toArray();
+  const quotes = await col.find({}).toArray();
 
   return Response.json({ quotes });
 }
