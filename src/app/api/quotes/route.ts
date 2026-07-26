c39aee96bd6a683ed230e7d33027174fb886ee54
busrakuscu0 <busrakuscu6991@gmail.com>
2026-07-26 18:23:17 +0200

    feat: update GET route to filter quotes by admin approval and remove seed functionality

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -4,8 +4,8 @@ import { Quote } from "@/types/quotes";
 export async function GET() {
   const db = await getDb();
   const col = db.collection<Quote>(Collections.quotes);
-
-  const quotes = await col.find({}).toArray();
+  const query = { adminApproved: true };
+  const quotes = await col.find(query).toArray();
 
   return Response.json({ quotes });
 }
