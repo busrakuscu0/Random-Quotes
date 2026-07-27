2af57bcd0c5382b7794de282fe78c642dc4ef274
busrakuscu0 <busrakuscu6991@gmail.com>
2026-07-27 04:05:46 +0200

    feat: add PUT route to update quotes in the database

--- src/app/api/quotes/route.ts
+++ src/app/api/quotes/route.ts
@@ -1,5 +1,6 @@
 import { Collections, getDb } from "@/lib/db";
 import { Quote } from "@/types/quotes";
+import { ObjectId } from "mongodb";
 
 export async function GET() {
   const db = await getDb();
@@ -8,4 +9,17 @@ export async function GET() {
   const quotes = await col.find({}).toArray();
 
   return Response.json({ quotes });
+}
+
+export async function PUT(request: Request) {
+  const body = await request.json();
+  const { _id, newQuote, newAuthor } = body;
+
+  const db = await getDb();
+  const col = db.collection(Collections.quotes);
+
+  await col.updateOne(
+    { _id: new ObjectId(_id) },
+    { $set: { quote: newQuote, author: newAuthor } },
+  );
 }
